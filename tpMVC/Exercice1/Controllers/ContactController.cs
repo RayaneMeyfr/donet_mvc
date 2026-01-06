3befddc1d83bacd185a9595e79d6073dcf5158b2
RayaneMeyfr <rayanemeyfr@hotmail.com>
2026-01-06 14:56:19 +0100

    exo1 2 et 3

---
+++ tpMVC/Exercice1/Controllers/ContactController.cs
@@ -0,0 +1,52 @@
+﻿using Exercice1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice1.Controllers
+{
+    public class ContactController : Controller
+    {
+        private static List<Contact> contacts = new List<Contact>
+        {
+            new Contact{Id = 1,Name = "Dupont",FirstName = "Jean",MobileNumber = "0601020304"},
+            new Contact{Id = 2, Name = "Martin", FirstName = "Sophie", MobileNumber = "0611223344"},
+            new Contact{Id = 3, Name = "Bernard", FirstName = "Lucas", MobileNumber = "0699887766"}
+        };
+
+        public IActionResult ListContacts()
+        {
+            return View(contacts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.Id == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["contact"] = contact;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(contact);
+            }
+
+            contacts.Add(contact);
+            return RedirectToAction("ListContacts");
+        }
+    }
+}
---
+++ tpMVC/Exercice1/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exercice1.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MobileNumber { get; set; }
+
+    }
+}
