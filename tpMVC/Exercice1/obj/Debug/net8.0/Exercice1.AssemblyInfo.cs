8c931124eb08484e6f0f0e19f9efd18b63a36681
RayaneMeyfr <rayanemeyfr@hotmail.com>
2026-01-06 15:40:35 +0100

    exo 4

--- tpMVC/Exercice1/obj/Debug/net8.0/Exercice1.AssemblyInfo.cs
+++ tpMVC/Exercice1/obj/Debug/net8.0/Exercice1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12da72f238e0378710d16752da8ef24dea36eca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3befddc1d83bacd185a9595e79d6073dcf5158b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ tpMVC/Exercice4/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Exercice4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice4.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static string RandomString(string chars, int length)
+        {
+            Random random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Description = "Portable 15 pouces, 16GB RAM", Category = "Informatique" },
+            new Product { Id = 2, Name = "Smartphone", Description = "Android 128GB, 6GB RAM", Category = "Téléphonie" },
+            new Product { Id = 3, Name = "Chaise de bureau", Description = "Ergonomique, noire", Category = "Mobilier" },
+            new Product { Id = 4, Name = "Cafetière", Description = "Machine à café filtre, 12 tasses", Category = "Cuisine" },
+            new Product { Id = 5, Name = "Sac à dos", Description = "Résistant à l'eau, 20L", Category = "Accessoires" }
+        };
+
+        public IActionResult ListProducts()
+        {
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = products.FirstOrDefault(c => c.Id == id);
+
+            return View(product);
+        }
+
+        public IActionResult CreateRandom()
+        {
+            int newId = products.Any() ? products.Max(p => p.Id) + 1 : 1;
+
+            Product product = new Product
+            {
+                Id = newId,
+                Name = RandomString("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 8),
+                Description = "Description " + RandomString("abcdefghijklmnopqrstuvwxyz", 12),
+                Category = RandomString("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 6)
+            };
+
+            products.Add(product);
+
+            return RedirectToAction("ListProducts");
+        }
+
+    }
+}
